e1315f73c37adbf3e346d31cd1cb010ec7966049
Cristian Consonni <cristian@balist.es>
2025-07-08 10:19:17 +0200

    Add dehumanize function

--- aliases/.aliases.d/01-aliases.sh
+++ aliases/.aliases.d/01-aliases.sh
@@ -129,3 +129,21 @@ fi
 function grephosts {
   grep "$@" /etc/hosts | grep -vE '^#'
 }
+
+# convert human readable to bytes in bash
+# https://stackoverflow.com/a/31625253/2377454
+function dehumanize {
+  for v in "${@:-$(</dev/stdin)}"; do
+  echo $v | awk 'BEGIN{IGNORECASE = 1}
+    function printpower(n,b,p) {printf "%u\n", n*b^p; next}
+    /[0-9]$/{print $1;next};
+    /K(iB)?$/{printpower($1,  2, 10)};
+    /M(iB)?$/{printpower($1,  2, 20)};
+    /G(iB)?$/{printpower($1,  2, 30)};
+    /T(iB)?$/{printpower($1,  2, 40)};
+    /KB$/{    printpower($1, 10,  3)};
+    /MB$/{    printpower($1, 10,  6)};
+    /GB$/{    printpower($1, 10,  9)};
+    /TB$/{    printpower($1, 10, 12)}'
+  done
+}
